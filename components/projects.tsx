104105bfb6f966dcb09e9fd758866a60c66665bf
g1mmethemoney <theguywholovecheetos@gmail.com>
2024-04-08 16:54:19 +0300

    fix threshold

--- components/projects.tsx
+++ components/projects.tsx
@@ -7,7 +7,7 @@ import Project from './project'
 import { useSectionInView } from '@/lib/hooks'
 
 export default function Projects() {
-  const { ref } = useSectionInView('Projects', 0.7)
+  const { ref } = useSectionInView('Projects', 0.75)
 
   return (
     <section ref={ref} id="projects" className="mb-10 sm:mb-0 scroll-mt-28">
