0ba412f6b5afa72317cf8c0d7e7091115067e1c7
g1mmethemoney <theguywholovecheetos@gmail.com>
2024-09-22 15:46:04 +0300

    change name

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small>
-        &copy; {new Date().getFullYear()} g1mmethemoney. All rights reserved.
+        &copy; {new Date().getFullYear()} wad4444. All rights reserved.
       </small>
       <p>Built with Next.Js, Typescript, Tailwind CSS and Lots Of ♥ ;)</p>
     </footer>
